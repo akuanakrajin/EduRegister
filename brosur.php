0136e79d60eff07b63c9b34b6a982cd9a245943f
Your Name <akunapacoba431@gmail.com>
2024-12-07 12:11:30 +0700

    first commit

---
+++ brosur.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PPDB - EduRegister</title>
+	<link rel="icon" href="img\logo-tj.png" type="image/png">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-5ZykD8k5SYR7rFw5UcLvOKviEqKxYHY+E5hJ/J4im5yJWkj1fPh82ZeiZTnUmJoc" crossorigin="anonymous">
+</head>
+
+<body>
+	<header>
+	<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #71FF8F;">
+	<img src="img/logo-tj-trn.png" alt="Logo" class="img-fluid" style="max-witdh: 100%; width: 5%; margin-left: 20px;">
+    <div class="container-md">
+        <a class="navbar-brand">EduRegister</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav flex-row me-3">
+                <li class="nav-item">
+                    <a class="nav-link me-3" href="index.php">Home</a>
+                </li>
+				<li class="nav-item">
+                    <a class="nav-link" href="brosur.php">Brosur</a>
+                </li>
+            </ul>
+            <form class="d-flex mx-2" action="form-daftar.php" method="post">
+                <button class="btn btn-outline-success" type="submit">Daftar Siswa</button>
+            </form>
+            <form class="d-flex mx-2" action="login.php" method="post">
+                <button class="btn btn-outline-dark" type="submit">Login admin</button>
+            </form>
+        </div>
+    </div>
+</nav>
+</header>
+
+    <h2 style="text-align: center; font-weight: bold; color: black; margin-top: 90px;">Berikut Info Lebih Lanjut</h2>
+	</div> <br> <br>
+	<img src="img/brosur1.jpg" style="max-width: 100%; width: 40%; margin-left: 30%;"> <br>
+	<hr style="border: 1px solid black; width: 80%; margin-left: 10%">
+	<img src="img/brosur2.jpg" style="max-width: 100%; width: 40%; margin-left: 30%;"> <br>
+	<hr style="border: 1px solid black; width: 80%; margin-left: 10%">
+	<img src="img/brosur3.jpg" style="max-width: 100%; width: 40%; margin-left: 30%; box-shadow: 2px 2px 2px black;"> <br> <br>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js" integrity="sha384-7N9IH81C29L8jk5U6FtP06gZ9sLnW6n4P0Tz+K5j6oO7/JctE+qyStl/FpmmTJ4Z" crossorigin="anonymous"></script>
+</body>
+</html>
